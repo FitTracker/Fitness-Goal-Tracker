2fc55e2528b8843fd54ebe19382d3b8cf5c45e32
Ajaalexandra <ajacederberg@yahoo.com>
2017-12-18 16:19:24 -0600

    feat: configure sass for all files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import "./App.css";
-import "../../styles/css/index.css";
+import Navbar from "../NavBar/Navbar";
 
+import "../../styles/css/styles.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <Navbar />
+        <header>
+          <h1 className="App-header">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: "rgba(3, 155, 229, 0.76)",
+    primary2Color: "#ffcdd2",
+    accent1Color: "#00bcd4",
+    primary3Color: "#f3e5f5"
+  }
+});
+ReactDOM.render(
+  <div className="container">
+    <BrowserRouter>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <App />
+      </MuiThemeProvider>
+    </BrowserRouter>
+  </div>,
+  document.getElementById("root")
+);
