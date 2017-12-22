e31ed7925b9176665849335110b6c6c074028430
Joe Warren <joe@Joes-MacBook-Pro.local>
2017-12-22 10:26:56 -0600

    chore: file name fix

--- src/components/Time/TimerComponent/TimerComponent.js
+++ src/components/Time/TimerComponent/TimerComponent.js
@@ -55,7 +55,7 @@ class TimerComponent extends Component {
   };
 
   render() {
-    console.log(this.state.secondsRemaining);
+    // console.log(this.state.secondsRemaining);
     let buttons;
     const { countingDown } = this.state;
 
