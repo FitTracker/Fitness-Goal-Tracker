8fdc90fa443f753603e1401320129d7f5789f54a
Joe Warren <joe@Joes-MacBook-Pro-2.local>
2018-01-03 12:04:07 -0600

    chore: add tests for positive numbers on timer input fields

---
+++ src/components/Time/StopwatchComponent/StopwatchComponent.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// test("two plus two is four", () => {
+//   expect(2 + 2).toBe(4);
+// });
+
+test("should start stopwatch on button click", () => {
+  expect(startStopwatch).toHaveBeenCalled();
+});
--- src/components/Time/StopwatchComponent/__tests__/StopwatchComponent.test.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { StopwatchComponent } from "../StopwatchComponent";
-
-test("should start stopwatch on button click", () => {
-  expect().toBe();
-});
