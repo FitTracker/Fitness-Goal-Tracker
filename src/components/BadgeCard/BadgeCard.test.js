fd594f0033b6bedf7d417c77a4317a2f190f0813
valentina abramova <valentina@v.local>
2018-01-04 09:32:25 -0600

    added unit tests tht pass

--- src/components/BadgeCard/BadgeCard.test.js
+++ src/components/BadgeCard/BadgeCard.test.js
@@ -2,12 +2,11 @@ import React from "react";
 import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import store from "../../ducks/store/configureStore";
-import renderer from "react-test-renderer";
 
 configure({ adapter: new Adapter() });
 
-import Goals from "./Goals";
+import BadgeCard from "./BadgeCard";
 
-test("Goals should render as expected", () => {
-  shallow(<Goals store={store} />);
+test("BadgeCard should render as expected", () => {
+  shallow(<BadgeCard store={store} />);
 });
--- src/components/FriendCard/FriendCard.test.js
+++ src/components/FriendCard/FriendCard.test.js
@@ -2,12 +2,11 @@ import React from "react";
 import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import store from "../../ducks/store/configureStore";
-import renderer from "react-test-renderer";
 
 configure({ adapter: new Adapter() });
 
-import Goals from "./Goals";
+import FriendCard from "./FriendCard";
 
-test("Goals should render as expected", () => {
-  shallow(<Goals store={store} />);
+test("FriendCard should render as expected", () => {
+  shallow(<FriendCard store={store} />);
 });
