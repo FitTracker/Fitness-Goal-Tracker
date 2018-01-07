6418e4de1a09eea9b6240d8ea77824bd771b175e
Joe Warren <joe.warren7@gmail.com>
2018-01-07 11:45:55 -0600

    chore: add About Links in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,66 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const Footer = () => (
-  <div>
-    <hr />
-    <div>
-      <div className="footer-container">
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <hr />
         <div>
-          <h1 className="footer-logo"> Fittr </h1>
-          <p className="copyright">© 2018 Fittr</p>
-        </div>
+          <div className="footer-container">
+            <div>
+              <h1 className="footer-logo">
+                <Link to="/dashboard"> Fittr </Link>
+              </h1>
+              <p className="copyright">© 2018 Fittr</p>
+            </div>
 
-        <div>
-          <p className="footer-title"> About </p>
-          <div className="about-container">
-            <div>About</div>
-            <div>Features</div>
-            <div>Mobile</div>
-          </div>
-        </div>
-        <div>
-          <p className="footer-title"> Follow </p>
-          <div className="follow-container">
             <div>
-              <a href="https://twitter.com/Fittr9" target="_blank">
-                Twitter
-              </a>
+              <p className="footer-title">About</p>
+              <div className="about-container">
+                <div>
+                  <Link to="/about">About</Link>
+                </div>
+                <div>
+                  <Link to="/features">Features</Link>
+                </div>
+                <div>
+                  <Link to="/mobile">Mobile</Link>
+                </div>
+              </div>
             </div>
             <div>
-              <a href="https://www.instagram.com/fittr9/" target="_blank">
-                Instagram
-              </a>
+              <p className="footer-title"> Follow </p>
+              <div className="follow-container">
+                <div>
+                  <a href="https://twitter.com/Fittr9" target="_blank">
+                    Twitter
+                  </a>
+                </div>
+                <div>
+                  <a href="https://www.instagram.com/fittr9/" target="_blank">
+                    Instagram
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="https://www.youtube.com/channel/UCAo8xnkNl2cQOo3gwt3scAw/featured"
+                    target="_blank"
+                  >
+                    YouTube
+                  </a>
+                </div>
+              </div>
             </div>
             <div>
-              <a
-                href="https://www.youtube.com/channel/UCAo8xnkNl2cQOo3gwt3scAw/featured"
-                target="_blank"
-              >
-                YouTube
-              </a>
+              <p className="footer-title"> Help </p>
+              <div className="help-container">
+                <div>
+                  <a href="mailto:fittracker1@gmail.com">Fittr Support</a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div>
-          <p className="footer-title"> Help </p>
-          <div className="help-container">
             <div>
-              <a href="mailto:fittracker1@gmail.com">Fittr Support</a>
+              <p className="footer-title"> More </p>
+              <div className="help-container">
+                <div>Local</div>
+                <div>Careers</div>
+                <div>Developers</div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div>
-          <p className="footer-title"> More </p>
-          <div className="help-container">
-            <div>Local</div>
-            <div>Careers</div>
-            <div>Developers</div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Footer;
