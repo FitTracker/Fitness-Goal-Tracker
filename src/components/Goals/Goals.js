1141ecfdb7b6623cb1722935518958baeb513f45
Joe Warren <joe.warren7@gmail.com>
2018-01-08 09:53:59 -0600

    chore: use internal photo for dashboard goals header and about map

--- src/components/Goals/Goals.js
+++ src/components/Goals/Goals.js
@@ -9,6 +9,7 @@ import { getCurrentGoalsAndData, completeGoal } from "../../ducks/reducer.js";
 
 import AddGoal from "../AddGoal/AddGoal";
 import BadgeCard from "../BadgeCard/BadgeCard";
+import header from "./runner-header.jpg";
 
 class Goals extends Component {
   componentDidMount() {
@@ -155,10 +156,7 @@ class Goals extends Component {
       <div>
         <div className="goals-container">
           <div className="all-pies">
-            <img
-              className="goals-header-img"
-              src="https://lh3.googleusercontent.com/dI6je65aSkgJhLgYVQ9dkIK8B3-CBVRI8ujM79_NiVZxRqWxlu0MaR-BwtQNoA-SIFb8jGNt2F5HkbfFZ9yb36_zIQ-Fop_9m2qSB8_cgN-zgVO5MWu2We62MNs_7VPERg4HmhB6kL2KrvN39PuC_7FwSc9EX93JgKXEvQdlQdLwsEaDL64jjNVOiBQSiz5zG_cq5uPVVDkiHD5AtZAq2M_Rn7IDJ0v_mnhKOP8In-UV-_ogPhxcd404BDaAEivvgfwmPpHh8QhOtvCCYtbfwwJTeXOKZ4eASCIhuOT66jJGPiJXHNqHDDlVTXnOHDZqsaoqSJJJQsL3RoXUM_h26gf05mVlr7uOGza2mZo4MJcjIoJ6NOl8c7vCLLs43ao2jTuQh0gF4oQlLwGedWlGyjOXcalOwl3MGym567oCZk3pGgTqv6jDOl0e2Bp1FCjrXbQfkc9AkiEdWN0qZqqj11KzJq7V6QjYCwCDyAbWyLGY_fcGway8kYvfUkRcg2GJMDAsisj3KE-9T_qmq_y0NzdHR5iMeeKi_vZ5fPD0SfizXqgF9owWqRKOaPHXOfjYkg3QExawPkrlThCuP2gII6P2DVTr_c5pIXAnzNit=w905-h197-no"
-            />
+            <img className="goals-header-img" src={header} />
             <Card style={{ width: "90%" }}>
               {
                 // <div className="goals-header">
